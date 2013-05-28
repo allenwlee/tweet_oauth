5813b7d7469f55f066144c2bd101c7ac30a4cb83
Apprentice <apprentice@devbootcamp.com>
2013-05-28 15:16:50 -0700

    last

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,7 +2,13 @@ get '/' do
   erb :index
 end
 
+# post '/' do
+#   Twitter.update(params[:tweet])
+#   erb :index
+# end
+
 get '/sign_in' do
+  
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   redirect request_token.authorize_url
 end
@@ -17,7 +23,11 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
+  
+  p "ACCESSS TOOKEENN!!!!"
+  p @access_token.params 
+  @user = User.create(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+  session[:user_id] = @user.id
   # at this point in the code is where you'll need to create your user account and store the access token
 
   erb :index
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // $("form").on('submit', function(e){
+  //   e.preventDefault();
+  //   $('form').hide();
+  //   $("<div id='patience'>YOUR TWEET IS TWITTERING</div>").appendTo("h1");
+  //   $.ajax({
+  //     type: "POST",
+  //     url:  "/",
+  //     data: $(this).serialize(),
+  //     success: console.log(this)
+  //   }).done(function(){
+  //     $('form#twitterpost').show();
+  //     $('form input[name=tweet]').val("");
+  //     $('#patience').hide();
+  //   });
+  // });
 });
